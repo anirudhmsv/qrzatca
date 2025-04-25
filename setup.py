00749b052a06ca53ade09c319652cfefe0934d2a
anirudhmsv <anirudhvadakkayil@gmail.com>
2025-04-25 19:21:09 +0530

    added readme and license

--- setup.py
+++ setup.py
@@ -7,13 +7,19 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='qrzatca',  # Package name
-    version='0.1.0',  # Initial release version
+    version='0.1.2',  # Initial release version
     description='A Python library to generate ZATCA-compliant QR codes for Saudi Arabian e-invoices.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='Anirudh msv',
     author_email='anirudhvadakkayil@gmail.com',
-    url='https://github.com/anirudhmsv/qrzatca.git',  # Your GitHub repo URL
+    url='https://github.com/anirudhmsv/qrzatca.git',
+    project_urls={
+        "Documentation": "https://github.com/anirudhmsv/qrzatca",
+        "Source": "https://github.com/anirudhmsv/qrzatca",
+        "Bug Tracker": "https://github.com/anirudhmsv/qrzatca/issues",
+    },
+    keywords="zatca, qr code, e-invoice, saudi arabia, vat, tax, compliance, zatca qr",
     packages=find_packages(),
     install_requires=[
         'qrcode',
@@ -24,6 +30,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         "Topic :: Software Development :: Libraries",
+        "Topic :: Office/Business :: Financial :: Accounting",
     ],
     python_requires='>=3.6',
 )
+
